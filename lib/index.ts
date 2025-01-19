84805f8f93cd2212a9485522e798a30b9e869d53
daniarepublic <imdaniarepublic@gmail.com>
2025-01-19 16:26:23 -0600

    patch smtg

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,19 @@
-export * from "./example-games/shape-cast.js"
+// core
+export * from "./game-base.js"
+export * from "./game-networked.js"
+export * from "./game-object-base.js"
 export * from "./keyboard-context.jsx"
+
+export * from "./three-helpers.js"
+export * from "./three/resource-managing.js"
+
+export * from "./rapier/collision-layers.js"
+export * from "./rapier/physics.js"
+
+
+// implementations
+export * from "./game-objects/hud.js"
+export * from "./game-objects/player.js"
+
+export * from "./example-games/gltf-load.js"
+export * from "./example-games/shape-cast.js"
